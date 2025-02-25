00504a5bfb24498e0a7aa8a84e4d263aa5e925d2
hsinnosuketw <hsinnosuketw@gmail.com>
2025-02-25 20:35:18 +0800

    Implement function q_new
    Implement a function generating new queue.
    Return NULL if memory allocation failed.
    
    Change-Id: If67d76923d4062a57810f34ea916090863f8b926

--- queue.c
+++ queue.c
@@ -7,7 +7,14 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *q = malloc(sizeof(struct list_head));
+
+    if (!q)
+        return NULL;
+
+    INIT_LIST_HEAD(q);
+
+    return q;
 }
 
 /* Free all storage used by queue */
